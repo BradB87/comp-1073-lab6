ec4f4d112da23541c87c1f38879734efbe85d258
ToraKill <122499319+ToraKill@users.noreply.github.com>
2023-10-15 13:54:40 -0400

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -28,22 +28,53 @@ thumbBar.addEventListener('click', function (event) {
 		var imgSrc = event.target.getAttribute('src');
 		displayImage(imgSrc);
 
-		// code here//
-		// Lab 6 STEP G: Call the clearWayfinding() function that you built below - and enjoy the result!
+	
+
 
 		// Lab 6 STEP A: Inside the thumbBar.onclick event handler function, and also inside the if statement, change the event.target CSS outline property to "5px solid red"
-		//  thumbBar.onclick{
-		// if()
-		// }
+		
+		//wes
+		document.getElementById('thumbBar').onclick = function(event) {
+			var target = event.target;
+			if (target.tagName === 'img') {
+	
+				var allImages = document.querySelectorAll('thumbBar');
+				allImages.forEach(function(img) {
+					img.classList.remove('outline');
+				});
 
+				target.classList.add('outline');
+			}};
+		
 
 
 
 		// Lab 6 STEP B: Next, change the event.target CSS position property to "relative"
-		document.event.target.style.position = "relative";
+
+		document.event.target.style.position = "relative";//This is how I would do it brad I have no clue as to how to chaange this
+
+
 		// Lab 6 STEP C: And then set the CSS z-index property to "10" so that the thumbnail clicked is on top of all the others
-
-
+//reused 6 E script and edited it to quite a degree// wes
+		function clearWayfinding() {
+			for (const imageName in image) {
+				const newImage = document.createElement('img');
+				newImage.setAttribute('src', `images/${image[imageName]}`);
+				newImage.setAttribute('alt', alts[imageName]);
+				thumbBar.appendChild(newImage);
+				newImage.addEventListener('click', (e) => {
+				
+					const allImages = document.querySelectorAll('.thumbBar img');
+					allImages.forEach((img) => {
+						
+						//add title to this topImage fucntion and make it work wes
+						img.classList.remove('topImage');});
+					e.target.classList.add('topImage');
+					displayImage.src = e.target.src;
+					displayImage.alt = e.target.alt;
+				});
+			}
+		}
 
 
 		// Lab 6 STEP D: Initialize and declare a var called 'thumbImgs' using the querySelectorAll method to grab all the IMG elements inside the .thumb-bar//brad
@@ -65,14 +96,27 @@ thumbBar.addEventListener('click', function (event) {
 
 		}
 		// Lab 6 STEP F: Inside the clearWayfinding function, for each thumbImgs IMG element, set the CSS outline-width property to "0", and the z-index property also to "0"
-		// clearWayfinding() {
+		//wes
+		
+		function clearWayfinding() {
+			const thumbImgs = document.querySelectorAll('.thumbBar img');
+		
+			thumbImgs.forEach((img) => {
+				img.classList.add('clear');
+				img.addEventListener('click', (e) => handleImageClick(e));});
+				function handleImageClick(e) {
+				thumbImgs.forEach((img) => {
+					img.classList.add('clear');});
+		
+				e.target.classList.remove('clear');
+				displayImage.src = e.target.src;
+				displayImage.alt = e.target.alt;
+			}}}
 
+			
+		// Lab 6 STEP G: Call the clearWayfinding() function that you built below - and enjoy the result!
+		clearWayfinding();
 
-
-
-		// }
-
-	}
 	// This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Image_gallery and https://davidwalsh.name/event-delegate
 
 
